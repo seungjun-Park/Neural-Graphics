0e2a5deb53b4e46551af90d3449b2fdd215a34d3
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-02-18 01:18:04 +0900

    modified configs.

--- modules/utils/activation.py
+++ modules/utils/activation.py
@@ -14,7 +14,7 @@ class Sine(nn.Module):
         self.eps = eps
 
     def forward(self, x):
-        return torch.sin(30 * x)
+        return torch.sin(x)
 
 
 class Cosine(nn.Module):
@@ -28,4 +28,4 @@ class Cosine(nn.Module):
         self.eps = eps
 
     def forward(self, x):
-        return torch.cos(30 * x)
+        return torch.cos(x)
