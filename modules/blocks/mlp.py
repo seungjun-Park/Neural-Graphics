4de1c2e8d401fd34c6e1d9d792fbbce66922ce45
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-04-10 12:47:56 +0900

    modified mlp.

--- modules/blocks/mlp.py
+++ modules/blocks/mlp.py
@@ -22,8 +22,8 @@ class MLP(nn.Module):
         self.use_conv = use_conv
 
         if use_conv:
-            self.fc1 = conv_nd(dim, self.in_channels, self.embed_dim, kernel_size=1, stride=1, padding=1)
-            self.fc2 = conv_nd(dim, self.embed_dim, self.in_channels, kernel_size=1, stride=1, padding=1)
+            self.fc1 = conv_nd(dim, self.in_channels, self.embed_dim, kernel_size=1, stride=1, padding=0)
+            self.fc2 = conv_nd(dim, self.embed_dim, self.in_channels, kernel_size=1, stride=1, padding=0)
 
         else:
             self.fc1 = nn.Linear(self.in_channels, self.embed_dim)
