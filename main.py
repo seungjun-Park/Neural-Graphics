7f2645f8bf75db9dfc91d2021c4e8508eb965a72
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-04-11 19:23:29 +0900

    modified configs.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import argparse
+import glob
+import cv2
 
+import torchvision
 from omegaconf import OmegaConf
 from pytorch_lightning.trainer import Trainer
 
@@ -57,5 +60,38 @@ def main():
     # trainer.test(model=model)
 
 
+def test():
+    parsers = get_parser()
+
+    opt, unknown = parsers.parse_known_args()
+
+    # init and save configs
+    configs = [OmegaConf.load(cfg) for cfg in opt.base]
+    cli = OmegaConf.from_dotlist(unknown)
+    config = OmegaConf.merge(*configs, cli)
+
+    # datamodule
+    # NOTE according to https://pytorch-lightning.readthedocs.io/en/latest/datamodules.html
+    # calling these ourselves should not be necessary but it is.
+    # lightning still takes care of proper multiprocessing though
+
+    model = instantiate_from_config(config.module).eval()
+
+    data_path = '../test'
+    file_names = glob.glob(f'{data_path}/*')
+    for i, name in enumerate(file_names):
+        img = cv2.imread(f'{name}', cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = torchvision.transforms.transforms.ToTensor()(img)
+        img = torchvision.transforms.transforms.Resize([512, 512])(img)
+        img = img.unsqueeze(0)
+        img = model(img)
+        if len(img.shape) == 4:
+            img = img[0]
+        img = torchvision.transforms.ToPILImage()(img)
+        img.save(f'{name}_{i}.png', 'png')
+
+
 if __name__ == '__main__':
     main()
+    # test()
