2816eda409b2c9a213ec1b24246a8a71e4330a21
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-05-08 00:08:03 +0900

    modified loss.

--- modules/loss/edge_perceptual.py
+++ modules/loss/edge_perceptual.py
@@ -12,6 +12,7 @@ class EdgePerceptualLoss(nn.Module):
     def __init__(self,
                  eips_config,
                  bdcn_weight: float = 1.1,
+                 l1_weight: float = 1.0,
                  lpips_weight: float = 1.0,
                  eips_weight: float = 1.0,
                  ):
@@ -20,6 +21,7 @@ class EdgePerceptualLoss(nn.Module):
         self.bdcn_weight = bdcn_weight
         self.lpips_weight = lpips_weight
         self.eips_weight = eips_weight
+        self.l1_weight = l1_weight
 
         self.lpips = LPIPS().eval()
         self.eips = EIPS(**eips_config).eval()
@@ -27,6 +29,7 @@ class EdgePerceptualLoss(nn.Module):
     def forward(self, inputs: torch.Tensor, targets: torch.Tensor, conds: torch.Tensor, split: str = "train",
                 threshold: float = 0.5) -> torch.Tensor:
         bdcn = bdcn_loss3(targets, inputs, threshold=threshold) * self.bdcn_weight
+        l1 = F.l1_loss(inputs, targets, reduction='mean') * self.l1_weight
         if inputs.shape[1] == 1:
             inputs = inputs.repeat(1, 3, 1, 1)
 
@@ -34,12 +37,13 @@ class EdgePerceptualLoss(nn.Module):
             targets = targets.repeat(1, 3, 1, 1)
 
         p_loss = self.lpips(inputs.contiguous(), targets.contiguous()).mean() * self.lpips_weight
-        eips_loss = self.eips(conds.contiguous(), targets.contiguous()).mean() * self.eips_weight
+        eips_loss = self.eips(conds.contiguous(), targets.contiguous()).mean()
 
-        loss = p_loss + eips_loss + bdcn
+        loss = p_loss + eips_loss * self.eips_weight + bdcn + l1
 
         log = {"{}/loss".format(split): loss.clone().detach(),
                "{}/bdcn_loss".format(split): bdcn.detach(),
+               "{}/l1_loss".format(split): l1.detach(),
                "{}/p_loss".format(split): p_loss.detach(),
                "{}/eips_loss".format(split): eips_loss.detach(),
                }
