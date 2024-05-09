6f869c9f2b02cc90ccf0792a1e723769c3486700
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-05-10 01:52:29 +0900

    modified loss.

--- modules/loss/edge_perceptual.py
+++ modules/loss/edge_perceptual.py
@@ -10,24 +10,21 @@ from models.classification import EIPS
 
 class EdgePerceptualLoss(nn.Module):
     def __init__(self,
-                 existence_weight: float = 1.1,
-                 strength_weight: float = 1.0,
-                 smoothing_weight: float = 1.0,
+                 cats_weight: Union[List[float], Tuple[float]] = (1.0, 0.0, 0.0),
+                 l1_weight: float = 1.0,
                  lpips_weight: float = 1.0,
                  ):
 
         super().__init__()
-        self.existence_weight = existence_weight
+        self.l1_weight = l1_weight
+        self.cats_weight = cats_weight
         self.lpips_weight = lpips_weight
-        self.strength_weight = strength_weight
-        self.smoothing_weight = smoothing_weight
 
         self.lpips = LPIPS().eval()
 
     def forward(self, preds: torch.Tensor, labels: torch.Tensor, split: str = "train", threshold: float = 0.8) -> torch.Tensor:
-        existence = existence_loss(preds, labels, threshold=threshold) * self.existence_weight
-        strength = strength_loss(preds, labels) * self.strength_weight
-        smoothing = smoothing_loss(preds, labels) * self.smoothing_weight
+        l1 = F.l1_loss(preds, labels, reduction='mean') * self.l1_weight
+        cats = cats_loss(preds, labels, weights=self.cats_weight)
 
         if preds.shape[1] == 1:
             preds = preds.repeat(1, 3, 1, 1)
@@ -37,12 +34,11 @@ class EdgePerceptualLoss(nn.Module):
 
         p_loss = self.lpips(preds.contiguous(), labels.contiguous()).mean() * self.lpips_weight
 
-        loss = p_loss + existence + strength + smoothing
+        loss = p_loss + l1 + cats
 
         log = {"{}/loss".format(split): loss.clone().detach(),
-               "{}/existence_loss".format(split): existence.detach().mean(),
-               "{}/strength_loss".format(split): strength.detach().mean(),
-               "{}/smoothing_loss".format(split): smoothing.detach().mean(),
+               "{}/cats_loss".format(split): cats.detach().mean(),
+               "{}/l1_loss".format(split): l1.detach().mean(),
                "{}/p_loss".format(split): p_loss.detach().mean(),
                }
 
