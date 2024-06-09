04018f854e5c3a83f221362f306d15575b5b7684
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-06-09 18:36:30 +0900

    modified configs.

--- modules/blocks/positional_encoding.py
+++ modules/blocks/positional_encoding.py
@@ -16,7 +16,7 @@ class PositionalEncoding(nn.Module):
         super().__init__()
 
         if isinstance(in_res, int):
-            in_res = list(in_res)
+            in_res = [in_res]
 
         if use_learnable_params:
             self.pos_enc = nn.Parameter(torch.zeros(1, in_channels, *in_res), requires_grad=True)
@@ -24,14 +24,15 @@ class PositionalEncoding(nn.Module):
             self.pos_enc = self._make_fixed_positional_encoding(in_channels, in_res)
 
     def _make_fixed_positional_encoding(self, in_channels: int, in_res: Union[List[int], Tuple[int]]) -> torch.Tensor:
-        encodings = torch.zeros(in_channels, *in_res)
-        position = torch.arange(0, np.prod(in_res), dtype=torch.float32).reshape(in_res).unsqueeze(0)
+        l = np.prod(in_res)
+        encodings = torch.zeros(l, in_channels)
+        position = torch.arange(0, l, dtype=torch.float32).unsqueeze(1)
         two_i = torch.arange(0, in_channels, 2, dtype=torch.float32)
         div_term = torch.exp(two_i * -(math.log(10000.0) / in_channels))
-        encodings[0::2, ...] = torch.sin(position * div_term)
-        encodings[1::2, ...] = torch.cos(position * div_term)
+        encodings[:, 0::2] = torch.sin(position * div_term)
+        encodings[:, 1::2] = torch.cos(position * div_term)
 
-        return encodings.unsqueeze(0)
+        return encodings.permute(1, 0).reshape(in_channels, *in_res).unsqueeze(0)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x + self.pos_enc
