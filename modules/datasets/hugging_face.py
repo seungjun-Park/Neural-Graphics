65a56149081ab289ada93e0201ea3476021a909b
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-07-10 16:36:00 +0900

    add hugginface dataset downloader.

---
+++ modules/datasets/hugging_face.py
@@ -0,0 +1,51 @@
+import glob
+import os
+import zipfile
+import shutil
+
+from huggingface_hub import hf_hub_download
+# from waifuc.source import LocalSource
+
+characters = ['ako', 'rio']
+dataset_path = '../../datasets/anime'
+
+for name in characters:
+    # download raw archive file
+    zip_file = hf_hub_download(
+        repo_id=f'CyberHarem/{name}_bluearchive',
+        repo_type='dataset',
+        filename='dataset-raw.zip',
+    )
+
+    # extract files to your directory
+    os.makedirs(f'{dataset_path}/{name}', exist_ok=True)
+    with zipfile.ZipFile(zip_file, 'r') as zf:
+        zf.extractall(f'{dataset_path}/{name}')
+
+    os.makedirs(f'{dataset_path}/train/{name}/edges', exist_ok=True)
+    os.makedirs(f'{dataset_path}/train/{name}/images', exist_ok=True)
+    os.makedirs(f'{dataset_path}/train/{name}/tags', exist_ok=True)
+
+    os.makedirs(f'{dataset_path}/val/{name}/edges', exist_ok=True)
+    os.makedirs(f'{dataset_path}/val/{name}/images', exist_ok=True)
+    os.makedirs(f'{dataset_path}/val/{name}/tags', exist_ok=True)
+
+    images = glob.glob(f'{dataset_path}/{name}/*.*')
+    tags = glob.glob(f'{dataset_path}/{name}/.*.*')
+
+    for i, (image, tag) in enumerate(zip(images, tags)):
+        if i < 5:
+            split = 'train'
+        else:
+            split = 'val'
+
+        tag, tag_form = tag.rsplit('.', 1)
+        tag_src, tag = tag.split('\\', 1)
+
+        image, image_form = image.rsplit('.', 1)
+        image_src, image = image.split('\\', 1)
+
+        shutil.move(f'{image_src}/{image}.{image_form}', f'{dataset_path}/{split}/{name}/images/{image}.{image_form}')
+        shutil.move(f'{tag_src}/{tag}.{tag_form}', f'{dataset_path}/{split}/{name}/tags/{tag}.{tag_form}')
+
+    os.rmdir(f'{dataset_path}/{name}')
