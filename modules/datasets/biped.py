4f3f6ec7ca4cc2e08cd3abe8bad8f504f85f58d4
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-07-25 15:25:50 +0900

    add BIPED v2 datasets & config.

--- modules/datasets/biped.py
+++ modules/datasets/biped.py
@@ -7,7 +7,8 @@ import cv2
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
-from utils import instantiate_from_config
+from utils import instantiate_from_config, to_2tuple
+from typing import Union, List, Tuple
 
 IMG_FORMATS = ['png', 'jpg']
 STR_FORMATS = ['txt', 'csv']
@@ -17,48 +18,68 @@ class BIPEDDataset(Dataset):
     def __init__(self,
                  root,
                  train=True,
-                 transform_configs=None,
-                 target_transform_configs=None,
+                 size: Union[int, List[int], Tuple[int]] = 224,
+                 scale: Union[List[float], Tuple[float]] = (0.08, 1.0),
+                 ratio: Union[List[float], Tuple[float]] = (0.75, 1.3333333333333333),
+                 color_space: str = 'rgb',
                  ):
-        if transform_configs is not None:
-            transform_list = list()
-            for transform_config in transform_configs['transforms']:
-                transform_list.append(instantiate_from_config(transform_config))
-            self.transform = transforms.Compose(transform_list)
+        super().__init__()
+        color_space = color_space.lower()
+        if color_space == 'rgb':
+            self.color_space = cv2.COLOR_BGR2RGB
+        elif color_space == 'rgba':
+            self.color_space = cv2.COLOR_BGR2RGBA
+        elif color_space == 'gray':
+            self.color_space = cv2.COLOR_BGR2GRAY
+        elif color_space == 'xyz':
+            self.color_space = cv2.COLOR_BGR2XYZ
+        elif color_space == 'ycrcb':
+            self.color_space = cv2.COLOR_BGR2YCrCb
+        elif color_space == 'hsv':
+            self.color_space = cv2.COLOR_BGR2HSV
+        elif color_space == 'lab':
+            self.color_space = cv2.COLOR_BGR2LAB
+        elif color_space == 'luv':
+            self.color_space = cv2.COLOR_BGR2LUV
+        elif color_space == 'hls':
+            self.color_space = cv2.COLOR_BGR2HLS
+        elif color_space == 'yuv':
+            self.color_space = cv2.COLOR_BGR2YUV
 
+        self.to_tensor = transforms.ToTensor()
+
+        self.size = list(to_2tuple(size))
+        self.scale = list(to_2tuple(scale))
+        self.ratio = list(to_2tuple(ratio))
+
+        self.resize = transforms.Resize(size=self.size, antialias=True)
+
+        if train:
+            self.edge_names = glob.glob(f'{root}/*/edge_maps/train/*/*.*')
+            self.img_names = glob.glob(f'{root}/*/imgs/train/*/*.*')
         else:
-            self.transform = None
-
-        if target_transform_configs is not None:
-            target_transform_list = list()
-            for target_transform_config in target_transform_configs['transforms']:
-                target_transform_list.append(instantiate_from_config(target_transform_config))
-            self.target_transform = transforms.Compose(target_transform_list)
-
-        else:
-            self.target_transform = None
-
-        root = os.path.join(root, 'edges')
-
-        split = 'train' if train else 'test'
-
-        self.edge_names = glob.glob(f'{root}/edge_maps/{split}/rgbr/real/*.*')
-        self.img_names = glob.glob(f'{root}/imgs/{split}/rgbr/real/*.*')
-
-        assert len(self.edge_names) == len(self.img_names)
+            self.edge_names = glob.glob(f'{root}/*/edge_maps/test/*/*.*')
+            self.img_names = glob.glob(f'{root}/*/imgs/test/*/*.*')
 
     def __getitem__(self, index):
         edge_name = self.edge_names[index]
         img_name = self.img_names[index]
 
         img = cv2.imread(f'{img_name}', cv2.IMREAD_COLOR)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, self.color_space)
         edge = cv2.imread(f'{edge_name}', cv2.IMREAD_GRAYSCALE)
 
-        if self.transform is not None:
-            img = self.transform(img)
-            edge = self.transform(edge)
-            edge = 1.0 - edge
+        img = self.to_tensor(img)
+        edge = self.to_tensor(edge)
+        edge = 1.0 - edge
+        # edge = torch.where(edge >= 0.8, 1.0, 0.0)
+        img = self.resize(img)
+        edge = self.resize(edge)
+
+        # i, j, h, w = transforms.RandomResizedCrop.get_params(img, scale=self.scale, ratio=self.ratio)
+        #
+        # img = tf.resized_crop(img, i, j, h, w, size=self.size, antialias=True)
+        # edge = tf.resized_crop(edge, i, j, h, w, size=self.size, antialias=True)
 
         return img, edge
 
