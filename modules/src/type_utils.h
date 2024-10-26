f3cd3bae3c9d45b94acad6617e7e5185732ee7d6
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-10-26 16:18:32 +0900

    add src.

---
+++ modules/src/type_utils.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <c10/core/ScalarType.h>
+#include <cuda_runtime.h>
+#include <cuda_fp16.h>
+#include <cuda_bf16.h>
+#include <type_traits>
+
+// to support half precision cuda version
+
+template<typename T>
+struct type_mapper
+{
+	using type = T;
+};
+
+#if defined(__CUDACC__) && (__CUDA_ARCH__ >= 700 || !defined(__CUDA_ARCH__))
+template<>
+struct type_mapper<c10::Half>
+{
+	using type = half;
+};
+#endif
+
+#if defined(__CUDACC__) && (__CUDA_ARCH__ >= 800 || !defined(__CUDA_ARCH__))
+template<>
+struct type_mapper<c10::BFloat16>
+{
+	using type = nv_bfloat16;
+};
+#endif
+
+template<typename T>
+using mapped_type = typename type_mapper<T>::type;
