0d86b8adfc2297878361ab8573bde8032aaae6bf
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-11-15 01:42:30 +0900

    modified enet.

--- models/edge/enet.py
+++ models/edge/enet.py
@@ -109,7 +109,7 @@ class EDNSE(pl.LightningModule):
         tb.add_image(f'{prefix}/{split}', x[0].float(), self.global_step, dataformats='CHW')
 
     def configure_optimizers(self) -> Any:
-        opt_net = torch.optim.AdamW(list(self.net.parameters())),
+        opt_net = torch.optim.AdamW(list(self.net.parameters()),
                                     lr=self.lr,
                                     weight_decay=self.weight_decay,
                                     betas=(0.5, 0.9)
