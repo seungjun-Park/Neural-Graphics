72f02760756bbcc4e7918e412aeed809d85aa141
Seungjun Park(박승준) <77137245+seungjun-Park@users.noreply.github.com>
2024-11-19 12:58:42 +0900

    modified main.py

--- main.py
+++ main.py
@@ -124,31 +124,30 @@ def test():
     # data_path = './datasets/arknights_v2/train/surtr/images'
     data_path = '/local_datasets/wakamo/val/images'
     file_names = glob.glob(f'{data_path}/*.*')
-    with torch.no_grad():
-        for i, name in enumerate(file_names):
-            print(i)
-            img = cv2.imread(f'{name}', cv2.IMREAD_COLOR)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = torchvision.transforms.transforms.ToTensor()(img).to(device)
-            img = torchvision.transforms.transforms.Resize(1024)(img)
-            c, h, w = img.shape
-            if w % 8 != 0:
-                w = math.ceil(w / 8) * 8
-            if h % 8 != 0:
-                h = math.ceil(h / 8) * 8
-            img = torchvision.transforms.transforms.Resize([h, w])(img)
-            img = img.unsqueeze(0)
-            img = model(img)
-            img = img.detach().cpu()
-            if len(img.shape) == 4:
-                img = img[0]
-            img = torchvision.transforms.ToPILImage()(img)
-            p1, p2 = name.rsplit('images', 1)
-            if not os.path.isdir(f'{p1}/edges_v2'):
-                os.mkdir(f'{p1}/edges_v2')
-            img.save(f'{p1}/edges_v2/{p2}.png', 'png')
-            # p1, p2 = name.rsplit('imgs', 1)
-            # img.save(f'{p1}/edge_maps/{p2}', 'png')
+    # with torch.no_grad():
+    #     for i, name in enumerate(file_names):
+    #         img = cv2.imread(f'{name}', cv2.IMREAD_COLOR)
+    #         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #         img = torchvision.transforms.transforms.ToTensor()(img).to(device)
+    #         img = torchvision.transforms.transforms.Resize(1024)(img)
+    #         c, h, w = img.shape
+    #         if w % 8 != 0:
+    #             w = math.ceil(w / 8) * 8
+    #         if h % 8 != 0:
+    #             h = math.ceil(h / 8) * 8
+    #         img = torchvision.transforms.transforms.Resize([h, w])(img)
+    #         img = img.unsqueeze(0)
+    #         img = model(img)
+    #         img = img.detach().cpu()
+    #         if len(img.shape) == 4:
+    #             img = img[0]
+    #         img = torchvision.transforms.ToPILImage()(img)
+    #         p1, p2 = name.rsplit('images', 1)
+    #         if not os.path.isdir(f'{p1}/edges_v2'):
+    #             os.mkdir(f'{p1}/edges_v2')
+    #         img.save(f'{p1}/edges_v2/{p2}.png', 'png')
+    #         # p1, p2 = name.rsplit('imgs', 1)
+    #         # img.save(f'{p1}/edge_maps/{p2}', 'png')
 
 
 if __name__ == '__main__':
